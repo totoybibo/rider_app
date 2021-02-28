9265f6c6d3ee09ad7a611df4c385d60e3315c5cb
Leobelle Joseph Villocog <leobellejosephvillocog@outlook.com>
2021-02-28 17:38:30 +0800

    Initial commit

---
+++ android/app/src/main/kotlin/app/totoybibo/rider_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package app.totoybibo.rider_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Main());
+}
+
+class Main extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
