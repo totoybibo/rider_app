c7a35fe122c468f0d940229c73340952bc96eaa5
Leobelle Joseph Villocog <leobellejosephvillocog@outlook.com>
2021-03-04 13:46:27 +0800

    divider change color to grey

--- lib/AllWidgets/divider.dart
+++ lib/AllWidgets/divider.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class DividerWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.white, width: double.infinity, height: 1);
+    return Container(color: Colors.grey, width: double.infinity, height: 1);
   }
 }
